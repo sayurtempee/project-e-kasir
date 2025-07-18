980aa39bcf4d24d1aebb8f02e13ce96b3ed2e112
kazuo <kazuo@kazuo-mii>
2025-07-18 18:01:30 +0700

    create new views alert

---
+++ resources/views/layouts/alert.blade.php
@@ -0,0 +1,11 @@
+    @if (session('success'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Berhasil!',
+                text: '{{ session('success') }}',
+                showConfirmButton: false,
+                timer: 2500
+            });
+        </script>
+    @endif
