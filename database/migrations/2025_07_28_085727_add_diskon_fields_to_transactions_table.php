d2f5d1f7459c39e287d2d9e5930f4cfd7f70fdc4
kazuo <kazuo@kazuo-mii>
2025-07-28 10:39:39 +0700

    menambah migrasi baru di fields diskon

---
+++ database/migrations/2025_07_28_085727_add_diskon_fields_to_transactions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->integer('diskon_poin')->nullable();
+            $table->decimal('diskon_persen', 5, 2)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropColumn(['diskon_poin', 'diskon_persen']);
+        });
+    }
+};
